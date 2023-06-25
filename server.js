f7a2c83782a10ce9151f80041a268d3a341e88fd
Abraar <reinhardt.codes@gmail.com>
2023-06-25 15:40:02 +0600

    Token validation

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 // Create a MySQL database connection
 const db = mysql.createConnection({
   host: 'localhost',
@@ -111,6 +112,29 @@ app.post('/login', (req, res) => {
       });
     });
   });
+
+
+
+// Middleware for token validation
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ message: 'Access token not found' });
+    }
+  
+    jwt.verify(token, 'your_secret_key', (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: 'Invalid or expired token' });
+      }
+      req.user = user;
+      next();
+    });
+  };
+  
+// Define protected route
+app.get('/protected', authenticateToken, (req, res) => {
+// Code for protected route
+});
 
   
 // Start the server
