ea75d62b29a414ed704a19a26eb66fbfb06362ed
Abraar <reinhardt.codes@gmail.com>
2023-07-03 15:58:19 +0600

    touchup and login page

--- public/js/app.js
+++ public/js/app.js
@@ -1,80 +1,87 @@
 // Fetch tasks from the backend and render them on the page
 async function fetchTasks() {
-  const response = await fetch('/tasks');
-  const tasks = await response.json();
+  try {
+    const response = await fetch('/tasks');
+    if (!response.ok) {
+      throw new Error('Failed to fetch tasks');
+    }
+    const tasks = await response.json();
 
-  const taskList = document.getElementById('taskList');
-  taskList.innerHTML = '';
+    const taskList = document.getElementById('taskList');
+    taskList.innerHTML = '';
 
-  tasks.forEach(task => {
-    const taskItem = document.createElement('li');
-    taskItem.classList.add('task-item');
+    tasks.forEach(task => {
+      const taskItem = document.createElement('li');
+      taskItem.classList.add('task-item');
 
-    const taskTitle = document.createElement('span');
-    taskTitle.textContent = task.title;
+      const taskTitle = document.createElement('span');
+      taskTitle.textContent = task.title;
 
-    const taskDescription = document.createElement('span');
-    taskDescription.textContent = task.description;
+      const taskDescription = document.createElement('span');
+      taskDescription.textContent = task.description;
 
-    const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-    deleteButton.addEventListener('click', () => deleteTask(task.id));
+      const deleteButton = document.createElement('button');
+      deleteButton.innerHTML = '<i class="fas fa-trash"></i> Delete';
+      deleteButton.addEventListener('click', () => deleteTask(task.id));
 
-    taskItem.appendChild(taskTitle);
-    taskItem.appendChild(taskDescription);
-    taskItem.appendChild(deleteButton);
+      taskItem.appendChild(taskTitle);
+      taskItem.appendChild(taskDescription);
+      taskItem.appendChild(deleteButton);
 
-    taskList.appendChild(taskItem);
-  });
-}
-
-// Add a new task to the backend and update the task list
-function addTask(event) {
-  event.preventDefault();
-
-  const titleInput = document.getElementById('title');
-  const descriptionInput = document.getElementById('description');
-
-  const task = {
-    title: titleInput.value,
-    description: descriptionInput.value
-  };
-
-  return fetch('/tasks', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(task)
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to add task');
-      }
-    })
-    .then(() => {
-      titleInput.value = '';
-      descriptionInput.value = '';
-      return fetchTasks();
-    })
-    .catch(error => {
-      console.error('Error adding task:', error);
+      taskList.appendChild(taskItem);
     });
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+  }
 }
 
-
-// Delete a task from the backend and update the task list
+// Function to delete a task
 async function deleteTask(taskId) {
-  await fetch(`/tasks/${taskId}`, {
-    method: 'DELETE'
-  });
-
-  fetchTasks();
+  try {
+    const response = await fetch(`/tasks/${taskId}`, { method: 'DELETE' });
+    if (!response.ok) {
+      throw new Error('Failed to delete task');
+    }
+    fetchTasks(); // Refresh the task list after deletion
+  } catch (error) {
+    console.error('Error deleting task:', error);
+  }
 }
 
-// Attach event listener to the form submission
-const taskForm = document.getElementById('taskForm');
-taskForm.addEventListener('submit', addTask);
+// Function to add a new task
+async function addTask(event) {
+  event.preventDefault();
+  const title = document.getElementById('title').value;
+  const description = document.getElementById('description').value;
 
-// Initial fetch and render of tasks
+  if (title.trim() === '' || description.trim() === '') {
+    alert('Please enter both title and description');
+    return;
+  }
+
+  const taskData = { title, description };
+  try {
+    const response = await fetch('/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(taskData),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to add task');
+    }
+
+    document.getElementById('taskForm').reset(); // Reset the form
+    fetchTasks(); // Refresh the task list after adding
+  } catch (error) {
+    console.error('Error adding task:', error);
+  }
+}
+
+// Event listener for form submission
+document.getElementById('taskForm').addEventListener('submit', addTask);
+
+// Initial fetch of tasks when the page loads
 fetchTasks();
